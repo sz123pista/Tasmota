1408cabfa25776d62354d5b86ce8eac667adf3b2
István Szabó <sz.pista@gmail.com>
2021-12-30 23:45:05 +0000

    Add support for multiple LM75 sensors
    Work in progress, not tested.

--- tasmota/xsns_26_lm75ad.ino
+++ tasmota/xsns_26_lm75ad.ino
@@ -29,6 +29,14 @@
 \*********************************************************************************************/
 
 #define XSNS_26                 26
+#define XSNS_120                120
+#define XSNS_121                121
+#define XSNS_122                122
+#define XSNS_123                123
+#define XSNS_124                124
+#define XSNS_125                125
+#define XSNS_126                126
+
 #define XI2C_20                 20  // See I2CDEVICES.md
 
 #define LM75AD_ADDRESS1					0x48
@@ -45,33 +53,31 @@
 #define LM75_THYST_REGISTER     0x02
 #define LM75_TOS_REGISTER       0x03
 
-bool lm75ad_type = false;
-uint8_t lm75ad_address;
+bool lm75ad_type[8] = {0};
 uint8_t lm75ad_addresses[] = { LM75AD_ADDRESS1, LM75AD_ADDRESS2, LM75AD_ADDRESS3, LM75AD_ADDRESS4, LM75AD_ADDRESS5, LM75AD_ADDRESS6, LM75AD_ADDRESS7, LM75AD_ADDRESS8 };
 
-void LM75ADDetect(void)
+void LM75ADDetect(uint32_t idx)
 {
-  for (uint32_t i = 0; i < sizeof(lm75ad_addresses); i++) {
-    lm75ad_address = lm75ad_addresses[i];
-    if (!I2cSetDevice(lm75ad_address)) {
-      continue; // do not make the next step without a confirmed device on the bus
-    }
-    uint16_t buffer;
-    if (I2cValidRead16(&buffer, lm75ad_address, LM75_THYST_REGISTER)) {
-      if (buffer == 0x4B00) {
-        lm75ad_type = true;
-        I2cSetActiveFound(lm75ad_address, "LM75AD");
-        break;
-      }
+  uint8_t lm75ad_address = lm75ad_addresses[idx];
+  if (!I2cSetDevice(lm75ad_address)) {
+    return; // do not make the next step without a confirmed device on the bus
+  }
+  uint16_t buffer;
+  if (I2cValidRead16(&buffer, lm75ad_address, LM75_THYST_REGISTER)) {
+    if (buffer == 0x4B00) {
+      lm75ad_type[idx] = true;
+      char name_buf[16] = {0};
+      snprintf(name_buf, sizeof(name_buf), "LM75AD_%d", idx);
+      I2cSetActiveFound(lm75ad_address, name_buf);
     }
   }
 }
 
-float LM75ADGetTemp(void)
+float LM75ADGetTemp(uint32_t idx)
 {
   int16_t sign = 1;
 
-  uint16_t t = I2cRead16(lm75ad_address, LM75_TEMP_REGISTER);
+  uint16_t t = I2cRead16(lm75ad_addresses[idx], LM75_TEMP_REGISTER);
   if (t & 0x8000) { // we are getting a negative temperature value
     t = (~t) +0x20;
     sign = -1;
@@ -80,18 +86,21 @@ float LM75ADGetTemp(void)
   return ConvertTemp(sign * t * 0.125);
 }
 
-void LM75ADShow(bool json)
+void LM75ADShow(uint32_t idx, bool json)
 {
-  float t = LM75ADGetTemp();
+  float t = LM75ADGetTemp(idx);
+
+  char name_buf[16] = {0};
+  snprintf(name_buf, sizeof(name_buf), "LM75AD_%d", idx);
 
   if (json) {
-    ResponseAppend_P(JSON_SNS_F_TEMP, "LM75AD", Settings->flag2.temperature_resolution, &t);
+    ResponseAppend_P(JSON_SNS_F_TEMP, name_buf, Settings->flag2.temperature_resolution, &t);
 #ifdef USE_DOMOTICZ
     if (0 == TasmotaGlobal.tele_period) DomoticzFloatSensor(DZ_TEMP, t);
 #endif  // USE_DOMOTICZ
 #ifdef USE_WEBSERVER
   } else {
-    WSContentSend_Temp("LM75AD", t);
+    WSContentSend_Temp(name_buf, t);
 #endif  // USE_WEBSERVER
   }
 }
@@ -100,28 +109,60 @@ void LM75ADShow(bool json)
  * Interface
 \*********************************************************************************************/
 
-bool Xsns26(uint8_t function)
+bool XsnsX(uint32_t idx, uint8_t function)
 {
   if (!I2cEnabled(XI2C_20)) { return false; }
 
   bool result = false;
 
   if (FUNC_INIT == function) {
-    LM75ADDetect();
+    LM75ADDetect(idx);
   }
-  else if (lm75ad_type) {
+  else if (lm75ad_type[idx]) {
     switch (function) {
       case FUNC_JSON_APPEND:
-        LM75ADShow(1);
+        LM75ADShow(idx, 1);
         break;
   #ifdef USE_WEBSERVER
       case FUNC_WEB_SENSOR:
-        LM75ADShow(0);
+        LM75ADShow(idx, 0);
         break;
   #endif  // USE_WEBSERVER
     }
   }
   return result;
+}
+
+bool Xsns26(uint8_t function) {
+  return XsnsX(0, function);
+}
+
+bool Xsns120(uint8_t function) {
+  return XsnsX(1, function);
+}
+
+bool Xsns121(uint8_t function) {
+  return XsnsX(2, function);
+}
+
+bool Xsns122(uint8_t function) {
+  return XsnsX(3, function);
+}
+
+bool Xsns123(uint8_t function) {
+  return XsnsX(4, function);
+}
+
+bool Xsns124(uint8_t function) {
+  return XsnsX(5, function);
+}
+
+bool Xsns125(uint8_t function) {
+  return XsnsX(6, function);
+}
+
+bool Xsns126(uint8_t function) {
+  return XsnsX(7, function);
 }
 
 #endif  // USE_LM75AD
